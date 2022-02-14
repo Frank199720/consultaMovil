a13cd1c42c7730f72a6c6d1635180e40630c78b8
Frank De La Cruz Urquiza <58741065+Frank199720@users.noreply.github.com>
2022-02-13 22:40:26 -0500

    screenshot and share wtsp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,17 +8,19 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AuthInterceptor } from './modules/authentication/helpers/auth.interceptor';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-
+import { Screenshot } from '@ionic-native/screenshot/ngx';
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule , HttpClientModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy } , 
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy } , 
     {
       provide : HTTP_INTERCEPTORS,
       useClass : AuthInterceptor,
       multi : true
-  }],
+  },Screenshot,SocialSharing],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/consulta/consulta.page.ts
+++ src/app/consulta/consulta.page.ts
@@ -3,58 +3,83 @@ import { ToastService } from '../helpers/plugins/toast.service';
 import { Persona } from './interfaces/persona.interface';
 import { ConsultaService } from './services/consulta.service';
 import { LoadingService } from '../helpers/plugins/loading.service';
+import { Screenshot } from '@ionic-native/screenshot/ngx';
 
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
 @Component({
   selector: 'app-consulta',
   templateUrl: './consulta.page.html',
   styleUrls: ['./consulta.page.scss'],
 })
 export class ConsultaPage implements OnInit {
+  public doi: string = '';
 
-  public doi : string = '';
+  public viewResult: boolean = false;
 
-  public viewResult : boolean = false;
-
-  public person:Persona = {
+  public person: Persona = {
     nombre_completo: '',
     codigo_verificacion: '',
     departamento: '',
     provincia: '',
     distrito: '',
-    direccion: ''
-  }
+    direccion: '',
+  };
 
-  constructor(private _toastService: ToastService , private _consultaService:ConsultaService , private _loadingService:LoadingService) { }
+  constructor(
+    private _toastService: ToastService,
+    private _consultaService: ConsultaService,
+    private _loadingService: LoadingService,
+    private _screenshot: Screenshot,
+    private socialSharing: SocialSharing
+  ) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  consulta(){
-    console.log('doi' , this.doi);
-    if(this.doi.length==0){
-      this._toastService.confirmMessage('Ingrese un DNI o RUC',2000);
-    }else{
+  consulta() {
+    console.log('doi', this.doi);
+    if (this.doi.length == 0) {
+      this._toastService.confirmMessage('Ingrese un DNI o RUC', 2000);
+    } else {
       this.viewResult = false;
       this._loadingService.presentLoading('Consultando...');
-      this._consultaService.getApiCliente(this.doi).subscribe((resp:any)=>{
-        this._loadingService.dismissLoader();
-        if(resp.success){
-          this.person = resp.data;
-          this.viewResult = true;
-        }else{
-          this._toastService.confirmMessage(resp.message,2000);
-        }      
-      },err=>{
-        this._loadingService.dismissLoader();
-        this._toastService.confirmMessage('Error de servidor',2000);
-      })
+      this._consultaService.getApiCliente(this.doi).subscribe(
+        (resp: any) => {
+          this._loadingService.dismissLoader();
+          if (resp.success) {
+            this.person = resp.data;
+            this.viewResult = true;
+          } else {
+            this._toastService.confirmMessage(resp.message, 2000);
+          }
+        },
+        (err) => {
+          this._loadingService.dismissLoader();
+          this._toastService.confirmMessage('Error de servidor', 2000);
+        }
+      );
     }
-    
-    
   }
-  refresh(){
+  screenshot() {
+    this._screenshot
+      .URI( 80)
+      .then(async (val) => {
+        const { URI } = val;
+        console.log('filepath', URI);
+        this.socialSharing.share(`Consulta DNI ${this.doi}: `, `${this.doi}.png`,URI)
+                            .then(val =>{
+                              console.log('valor al compartir',val)
+                            })
+                            .catch(err=>{
+                              console.log('error al compartir: ',JSON.stringify(err))
+                            })
+        
+      })
+      .catch((err) => {
+        console.log('err', err);
+      });
+  }
+  refresh() {
     this.doi = '';
     this.viewResult = false;
   }
-
 }
